adadf2d48b866fe17de9db367f238f6e36631110
Rajesh-Dehury <56267680+Rajesh-Dehury@users.noreply.github.com>
2020-06-06 10:59:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 
 
 // APP CONFIG
-mongoose.connect("mongodb://localhost:27017/restfull_blog_app",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://rajesh:rajaraja@cluster0-nx9xd.mongodb.net/restfull_blog_app?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true});
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
